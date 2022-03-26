c69fb7722735f69525acd56487a17cd3a4dbe42e
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-03-26 18:04:22 +0600

    UI design complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,12 +12,15 @@ const Home = () => {
     return (
         <div className="row container mx-auto pt-5">
             <div className="col-9 border">
+                <div className='row row-cols-1 row-cols-sm-1 row-cols-md-3 g-4'>
                 {
                     items.map(item => <Item
                         key={item.id}
                         item = {item}
                     ></Item>)
                 }
+                </div>
+                
             </div>
             <div className="col-3 border">col-4</div>
         </div>
